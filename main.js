3a8f4a3c478287719b9cd6ad1dd12ad4a14a8349
Seif El-Deen Elattar <38303430+seifeldeendev@users.noreply.github.com>
2024-06-06 00:14:09 +0300

    Update main.js

--- main.js
+++ main.js
@@ -51,7 +51,7 @@ function changeLanguage(language) {
 }
 
 function loadData() {
-  fetch("../data.json")
+  fetch("https://seifeldeendev.github.io/NubaFresh/data.json")
   // fetch("../data.json")
     .then((response) => response.json())
     .then((data) => {
